b9b5b6949f86d0cc18150ea755926c802b5158a4
Izhar Ahmad <thisisizharahmad@gmail.com>
2023-03-14 02:13:47 +0500

    Add support for bplate_config.json

--- bplate/__main__.py
+++ bplate/__main__.py
@@ -28,6 +28,7 @@ import click
 import pathlib
 import shutil
 import os
+import json
 
 
 @click.group()
@@ -36,30 +37,55 @@ def cli():
 
 
 @cli.command()
-@click.argument('name')
 @click.argument('path', type=click.Path(exists=True, file_okay=False, dir_okay=True, readable=True))
-def new(name: str, path: str):
+def new(path: str):
     """Creates and stores a new boilerplate template.
 
     The boilerplate template will be available for later usage
     using the `bplate init` command.
 
-    The NAME argument represents the name of boilerplate. It is recommended
-    to use all lowercase characters in names and use dashes instead of spaces.
-
     The PATH argument is the path where boilerplate code is located. The provided
     path must point to a readable directory and not a file.
     """
-    click.secho('[i] Creating new boilerplate...')
+    click.secho('Creating new boilerplate...')
 
     boilerplates = core.ensure_bplate_data_dir('boilerplates')
     target = pathlib.Path(path)
+
+    name = None
+    config = None
+    cfg_path = os.path.join(target, 'bplate_config.json')
+
+    if os.path.exists(cfg_path):
+        click.secho('bplate_config.json found in root directory, opening.')
+        try:
+            with open(cfg_path, 'r') as f:
+                config = json.loads(f.read())
+        except Exception as f:
+            raise core.click_error('Failed to load blate_config.json. The file may be malformed or not openable.')
+        else:
+            name = config.get('name')
+            if name:
+                click.secho('Config successfully loaded.')
+    if not name:
+        if config:
+            click.secho('Failed to get boilerplate from bplate_config.json')
+        name = click.prompt('[i] Boilerplate name')
+
+    assert name is not None
     data = boilerplates.joinpath(name)
 
     if data.exists():
-        raise core.click_error('Another boilerplate with name %r already exists.' % name)
+        msg = click.style('[!] Another boilerplate with name %r exists, Overwrite?', fg='red', bold=True)
+        click.confirm(msg, abort=True)
+        shutil.rmtree(data)
 
-    click.secho('Copying files from %s to %s...' % (target, data), fg='blue')
+    if config is None:
+        click.echo('Creating bplate_config.json...')
+        with open(cfg_path, 'w') as f:
+            json.dump({'name': name}, f, indent=4)
+
+    click.secho('Copying files from %s to %s...' % (target, data))
     data.mkdir()
 
     files_copied = 0
@@ -84,8 +110,15 @@ def new(name: str, path: str):
             shutil.copy2(target_path, os.path.join(data_path, name))
             files_copied += 1
 
-    click.secho(f'Successfully copied {files_copied} files.', fg='blue')
+    click.secho(f'Successfully copied {files_copied} files.')
+
+    if config is None:
+        click.secho('[i] A "bplate_config.json" file has been created in root directory that ' \
+                    'can be edited to customize the information and generation config of boilerplate.',
+                    fg='blue')
+
     click.secho('[!] Boilerplate created successfully.\n', fg='green')
+
 
 @cli.command('delete')
 @click.argument('name')
@@ -106,6 +139,7 @@ def delete(name: str):
 
     click.secho('[!] Deleted boilerplate %r successfully.' % name, fg='green')
 
+
 @cli.command('list')
 def list_():
     """Shows the list of all available boilerplates."""
@@ -114,6 +148,8 @@ def list_():
 
     for item in target.iterdir():
         if not item.is_dir() or item.name.startswith('_'):
+            continue
+        if not os.path.exists(os.path.join(item, 'bplate_config.json')):
             continue
 
         click.secho(f'* {item.name}')
